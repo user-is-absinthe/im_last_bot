83cb6e7883b2185333dbee44dee000d7c37bdfda
AntipatternCorp <notmail@fake.com>
2019-03-01 12:19:03 +0300

    update cursors

--- end/client/cl_func.py
+++ end/client/cl_func.py
@@ -2,36 +2,47 @@ import sqlite3
 
 conn = sqlite3.connect("mydatabase.db") # или :memory: чтобы сохранить в RAM
 cursor = conn.cursor()
+# cursor.execute("""create table if not exists ClientList
+#                   (id int, nickname text, tgname text, id_chat text, message text)
+#                """)
+# cursor.execute("""create table if not exists Banner
+#                   (firma text, text_b text, period int)
+#                """)
 
 # Создание таблицы
+def start_defaullt():
+    conn = sqlite3.connect("mydatabase.db")
+    cursor = conn.cursor()
+    cursor.execute("""create table if not exists ClientList
+                      (id int, nickname text, tgname text, id_chat text, message text)
+                   """)
+    cursor.execute("""create table if not exists Banner
+                      (firma text, text_b text, period int)
+                   """)
+    conn.commit()
 
-cursor.execute("""create table if not exists ClientList
-                  (number int, nickname text, tgname text, id text, message text)
-               """)
-cursor.execute("""create table if not exists Banner
-                  (firma text, text_b text, period int)
-               """)
 
-
-def insert_data (table_name, data): # данные одной строкой через запятую по столбцам таблицы
+def insert_data (table_name, data): # данные в виде insert_data("ClientList", ''' 1, 'test', 'тест', 'f123', 'hello AN' ''')
     sql = 'insert into ' + str(table_name) + ' values ( ' + str(data) + ' )'
     cursor.execute(sql)
-    pass
+    conn.commit()
 
-def count (type):
+
+def count ():
     cursor.execute('select count(*) from ClientList')
-    count_list = cursor.fetchall()
-    return count_list
+    count_l = cursor.fetchall()
+    count_l = count_l[0][0]
+    return count_l
 
-def drop_data (table_name, num): # удаление выбывших и upgrade номеров
-    sql = 'delete from ' + str(table_name) + ' where number = ' + str(num)
+def drop_data (table_name, num): # удаление выбывших
+    sql = 'delete from ' + str(table_name) + ' where id = ' + str(num)
     cursor.execute(sql)
-    count_l = int(count())
-    for i in range(num+1, count_l):
-        sql = 'update ' + str(table_name) + ' set number = ' + str(i-1) + ' where number = ' + str(i)
-        cursor.execute(sql)
-
-    pass
+    conn.commit()
+    # count_l = int(count())
+    # for i in range(num+1, count_l):
+    #     sql = 'update ' + str(table_name) + ' set number = ' + str(i-1) + ' where number = ' + str(i)
+    #     cursor.execute(sql)
+    # conn.commit()
 
 
 
@@ -47,7 +58,7 @@ def all_client ():
     return rows
 
 def first_client ():
-    sql = "select * from ClientList where number = 1"
+    sql = 'select * from ClientList' # where id = 1
     cursor.execute(sql)
-    row = cursor.fetchall()
+    row = cursor.fetchone()
     return row
