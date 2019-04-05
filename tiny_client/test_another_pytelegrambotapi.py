1f47baffc8b733e98898f9d11b0a1fbd7eb9ef2c
owl <owl@owlmail.com>
2019-04-05 10:30:24 +0300

    failed test (/reg)

--- tiny_client/test_another_pytelegrambotapi.py
+++ tiny_client/test_another_pytelegrambotapi.py
@@ -32,23 +32,31 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'help', 'reg', 'count', 'word'])
 def start_help(message):
+    logging.info('User {0} (user id is {1}) send {2}.'.format(
+        '@' + message.from_user.username,
+        message.from_user.id,
+        message.text
+    ))
     if message.text == '/start':
-        to_send = ''
+        to_send = 'Доброго времени суток.\nДля продолжения работы Вам необходимо зарегистрироваться.' + \
+                  '\nДля этого используйте команду /reg.'
         # bot.send_message(message.from_user.id, 'first message')
-        bot.reply_to(message, "first msg")
+        bot.reply_to(message, to_send)
     elif message.text == '/help':
         # TODO: описание команд
         to_send = ''
         bot.send_message(message.from_user.id, 'help')
     elif message.text == '/reg':
-        to_send = 'Вы выбрали регистрацию.\nВведите имя. Выбирайте мудро.'
+        to_send = 'Вы выбрали регистрацию.\nПожалуйста, укажите имя. ' \
+                  'Выбирайте мудро, так как вы будете видны под этим именем окружающим.'
         bot.send_message(message.from_user.id, to_send)
+        # bot.reply_to(message, to_send)
         bot.register_next_step_handler(message, get_name)
     elif message.text == '/count':
         # сколько человек до тебя в очереди, предыдущий
         pass
     elif message.text == '/word':
-        to_send = ''
+        to_send = 'Введите секретное слово.'
         bot.send_message(message.from_user.id, to_send)
         # bot.register_next_step_handler(message, FUNC_NAME)
 
@@ -59,20 +67,28 @@ def get_text_message(message):
     # print(message.text)
     bot.send_message(message.from_user.id, 'Для регистрации введите команду /reg.')
 
-    if message.text == '/reg':
-        bot.send_message(message.from_user.id, 'Представтесь, пожалуйста.\nИмя увидит преподаватель, выбирайте мудро.')
-        bot.register_next_step_handler(message, get_name)
+    # if message.text == '/reg':
+    #     bot.send_message(message.from_user.id, 'Представтесь.\nИмя увидит преподаватель, выбирайте мудро.')
+    #     bot.register_next_step_handler(message, get_name)
 
 
 def get_name(message):
-    global name
+    # global name
     name = message.text
-    bot.send_message(message.from_user.id, 'Укажите причину для посещения.')
-    bot.register_next_step_handler(message, reason)
+    to_send = 'Вас зовут {0}, верно?\n Если да, то нажмите /yes, иначе /no'.format(name)
+    bot.reply_to(message, message=to_send)
+
+    if message.text == '/yes':
+        bot.send_message(message, 'Укажите причину для посещения.')
+        bot.register_next_step_handler(message, reason)
+    elif message.text == '/no':
+        bot.reply_to(message, 'Введите еще раз.')
+        bot.register_next_step_handler(message, get_name)
 
 
 def reason(message):
     global user_reason
+    name = 'A'
     user_reason = message.text
     bot.send_message(message.from_user.id, 'отлично. но у нас нет связи с внутренней логикой, поэтому...')
 
